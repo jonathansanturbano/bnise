aba41ab774fed5ec8e24a1870426549be50e6f12
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-09-25 16:44:49 +0200

    contact email switch

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,7 +9,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "Contact Form Inquiry",
-      :to => "james.what.a.giant.peach@gmail.com",
+      :to => "bnicevlways@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
   end
