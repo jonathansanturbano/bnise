1f23e0edf5c40faca8d94a6e82bfec698724905d
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-10-01 14:18:55 +0200

    flashes and confirmable user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :baskets
   has_many :artworks
   has_many :orders, through: :baskets
