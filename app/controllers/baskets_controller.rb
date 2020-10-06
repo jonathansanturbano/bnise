69735cb60177fcb40988a748cb1629512f60949e
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-10-06 15:18:54 +0200

    styling Brandon

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,4 +1,9 @@
 class BasketsController < ApplicationController
   def show
+    if @basket.basketItems.count > 0
+      @basket.basketItems.each do |basket_item|
+        basket_item.destroy if basket_item.artwork.sold == true
+      end
+    end
   end
 end
