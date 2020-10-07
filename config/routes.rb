90bea00c3d46af0f5d3d2981030e7eea31868855
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-10-07 14:30:57 +0200

    again

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   get '/404', to: "errors#not_found"
   get '/422', to: "errors#unacceptable"
-  get '/404', to: "errors#server_errors"
+  get '/500', to: "errors#server_errors"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
