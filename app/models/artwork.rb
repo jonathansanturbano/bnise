58f7cd5cfafc48b12acfd5b739537eb41d9b578f
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-10-07 13:33:53 +0200

    remove description validation from Artwork model

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,7 +1,6 @@
 class Artwork < ApplicationRecord
   has_many_attached :photos
   validates :name, presence: true, uniqueness: true
-  validates :description, presence: true, uniqueness: true, length: { minimum: 10 }
   validates :price, presence: true
   validates :dimensions, presence: true
   validates :material, presence: true
