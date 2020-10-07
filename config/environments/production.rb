37794111a2a3fa3ffccb867ff827ae0f17c9dee7
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-10-07 16:57:21 +0200

    fix mailer options in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "bnise_production"
 
+  config.action_mailer.default_url_options = { host: 'bnise.herokuapp.com' }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
