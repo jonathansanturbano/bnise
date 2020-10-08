947549381a119cb85634b4bf59d9e2de30e14c43
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-10-08 11:47:54 +0200

    check if item already sold before payment new page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,45 +7,59 @@ class OrdersController < ApplicationController
   end
 
   def create
-    line_items = []
 
-    total_price = 0
-
-    @basket.basketItems.each do |item|
-      total_price += item.artwork.price
-    end
-
-    order = Order.find_by(basket: @basket)
-
-    if order.nil?
-      order = Order.create!(amount: total_price, state: "pending", basket: @basket)
-    else
-      order.update(amount: total_price)
-    end
+    sold_artworks = []
 
     @basket.basketItems.each do |item|
-      new_item = {
-        name: item.artwork.name,
-        # images: ["https://bnise.herokuapp.com/assets/white_logo_cropped-834dfd6d8fb58736072fa5820f673595570c8476b134852daa3369ab5000e7d0.png"],
-        amount: item.artwork.price_cents,
-        currency: 'eur',
-        quantity: 1
-      }
-      line_items << new_item
+      sold_artworks << item.name && item.destroy if item.artwork.sold == true
     end
 
-    session = Stripe::Checkout::Session.create(
-      payment_method_types: ['card'],
-      line_items: line_items,
-      shipping_address_collection: {
-          allowed_countries: ['FR'],
+    if sold_artworks.empty?
+
+      line_items = []
+
+      total_price = 0
+
+      @basket.basketItems.each do |item|
+        total_price += item.artwork.price
+      end
+
+      order = Order.find_by(basket: @basket)
+
+      if order.nil?
+        order = Order.create!(amount: total_price, state: "pending", basket: @basket)
+      else
+        order.update(amount: total_price)
+      end
+
+      @basket.basketItems.each do |item|
+        new_item = {
+          name: item.artwork.name,
+          # images: ["https://bnise.herokuapp.com/assets/white_logo_cropped-834dfd6d8fb58736072fa5820f673595570c8476b134852daa3369ab5000e7d0.png"],
+          amount: item.artwork.price_cents,
+          currency: 'eur',
+          quantity: 1
+        }
+        line_items << new_item
+      end
+
+      session = Stripe::Checkout::Session.create(
+        payment_method_types: ['card'],
+        line_items: line_items,
+        shipping_address_collection: {
+          allowed_countries: ['FR']
         },
-      success_url: order_url(order),
-      cancel_url: basket_url(@basket)
-    )
+        success_url: order_url(order),
+        cancel_url: basket_url(@basket)
+      )
 
-    order.update(checkout_session_id: session.id)
-    redirect_to new_order_payment_path(order)
+      order.update(checkout_session_id: session.id)
+      redirect_to new_order_payment_path(order)
+    else
+      a = "is" if sold_arts.count == 1
+      a = "are" if sold_arts.count > 1
+      redirect_to basket_path(@basket), notice: "Sorry #{arts.join(',')} #{a} gone}"
+    end
   end
 
 end
